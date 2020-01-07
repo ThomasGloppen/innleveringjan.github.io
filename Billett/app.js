3ab4b91f3f48e8ac49f1bc34bd50ad0c9c8eb882
Thomas Gloppen <thgloppen@gmail.com>
2020-01-07 14:13:15 +0100

    tvev

--- Billett/app.js
+++ Billett/app.js
@@ -1,5 +1,5 @@
 // @ts-check
-const CONNECTSTRING = "postgres://shop:123@localhost/shop";
+const CONNECTSTRING = "postgres://bib:123@localhost/billett";
 const PORT = 3000;
 const express = require("express");
 const pgp = require("pg-promise")();
